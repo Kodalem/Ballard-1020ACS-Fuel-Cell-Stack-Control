f156e5ee803da00e7ea5886ab5e643c36334ddeb
Christine Elizabeth Koppel <kuutruu@posteo.net>
2023-12-28 22:16:31 +0200

    Second commit:
    UART Implementation
    I2C Setup
    Multicore Setup
    
    Todo:
    BME688 implementation finish

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 #![no_std]
 #![no_main]
 
+use bme680::*;
 use bsp::entry;
+use core::fmt;
+use core::fmt::Write;
+use core::time::Duration;
 use defmt::*;
 use defmt_rtt as _;
+
+use embedded_hal::blocking::delay;
+use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::blocking::i2c;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::ToggleableOutputPin;
 use panic_probe as _;
+// Pull in any important traits
+use rp_pico::hal::prelude::*;
+
+use cortex_m::delay::Delay;
+use hal::gpio::Pins;
+use hal::multicore::{Multicore, Stack};
+
+// A shorter alias for the Hardware Abstraction Layer, which provides
+// higher-level drivers.
+use rp_pico::hal;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
@@ -14,11 +33,25 @@ use rp_pico as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
+    fugit::RateExtU32,
     pac,
     sio::Sio,
+    uart::{DataBits, StopBits, UartConfig},
     watchdog::Watchdog,
 };
 use rp_pico::hal::bsp_pins;
+
+/// Stack for core 1
+///
+/// Core 0 gets its stack via the normal route - any memory not used by static
+/// values is reserved for stack and initialised by cortex-m-rt.
+/// To get the same for Core 1, we would need to compile everything seperately
+/// and modify the linker file for both programs, and that's quite annoying.
+/// So instead, core1.spawn takes a [usize] which gets used for the stack.
+/// NOTE: We use the `Stack` struct here to ensure that it has 32-byte
+/// alignment, which allows the stack guard to take up the least amount of
+/// usable RAM.
+static mut CORE1_STACK: Stack<4096> = Stack::new();
 
 fn current_purge_calculation(desired_current: u32) -> u32 {
     // Check if the current is under the fuel cell limit (75Amps)
@@ -38,7 +71,7 @@ fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let mut sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
@@ -62,6 +95,44 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+
+    // Multi-core setup
+    let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
+    let cores = mc.cores();
+    let core1 = &mut cores[1];
+
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gpio0.into_function(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gpio1.into_function(),
+    );
+    let mut uart = bsp::hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(9600.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    // Configure two pins as being I²C, not GPIO
+    let sda_pin = pins.gpio20.into_function::<hal::gpio::FunctionI2C>();
+    let scl_pin = pins.gpio21.into_function::<hal::gpio::FunctionI2C>();
+
+    info!("I2C pins configured");
+
+    // Create the I²C driver, using the two pre-configured pins. This will fail
+    // at compile time if the pins are in the wrong mode, or if this I²C
+    // peripheral isn't available on these pins!
+    let i2c = hal::I2C::i2c0(
+        pac.I2C0,
+        sda_pin,
+        scl_pin,
+        400.kHz(),
+        &mut pac.RESETS,
+        &clocks.peripheral_clock,
+    );
+
+    info!("I2C driver created");
 
     // Set the relay control pin
     let mut relay_control_pin = pins.gpio22.into_push_pull_output();
@@ -89,12 +160,33 @@ fn main() -> ! {
     relay_control_pin.set_high().unwrap();
     // Print the purge relay is on
     info!("Purge relay is on");
+    // Send data over UART that the purge relay is on
+    writeln!(uart, "ON\r\n").unwrap();
     // Delay for the purge setup time
     delay.delay_ms(purge_setup_time);
     // Turn off the purge relay
     relay_control_pin.set_low().unwrap();
+    // Send data over UART that the purge relay is off
+    writeln!(uart, "OFF\r\n").unwrap();
     // Print the purge relay is off
     info!("Purge relay is off");
+
+    uart.write_full_blocking(b"UART example\r\n");
+
+    let mut value = 0u32;
+
+    // Start the second core running the communication and temperature measurement loop
+    info!("Starting core 1");
+    core1
+        .spawn(unsafe { &mut CORE1_STACK.mem }, move || {
+            // Get the second core's copy of the `CorePeripherals`, which are per-core.
+            // Unfortunately, `cortex-m` doesn't support this properly right now,
+            // so we have to use `steal`.
+            let core = unsafe { pac::CorePeripherals::steal() };
+            // Loop the temperature measurement and communication
+            loop {}
+        })
+        .unwrap();
 
     // Print the purge valve control runtime sequence and loop forever
     info!("Purge valve control runtime sequence");
@@ -103,12 +195,17 @@ fn main() -> ! {
         relay_control_pin.set_high().unwrap();
         // Print the purge relay is on
         info!("Purge relay is on");
+        // Send data over UART that the purge relay is on
+        writeln!(uart, "ON\r\n").unwrap();
         // Delay for the purge duration
         delay.delay_ms(purge_duration);
+
         // Turn off the purge relay
         relay_control_pin.set_low().unwrap();
         // Print the purge relay is off
         info!("Purge relay is off");
+        // Send data over UART that the purge relay is off
+        writeln!(uart, "OFF\r\n").unwrap();
         // Delay for the purge time
         delay.delay_ms(purge_time);
     }
